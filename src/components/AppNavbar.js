c15e6137ceba32daf7203f2092069430d5e1c37e
Jayson <jaysonmsaavedra@gmail.com>
2018-01-30 17:14:25 -0700

    Got basic routing working with dummy info on each page

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 
+import Routes from './Routes.js'
+
 import { Menu, Segment }    from 'semantic-ui-react'
+import { Link }             from 'react-router-dom'
 
 export default class AppNavbar extends Component {
   state =  { activeItem: 'skills'}
@@ -13,16 +16,16 @@ export default class AppNavbar extends Component {
     return (
       <div>
         <Menu pointing secondary>
-          <Menu.Item name='skills' active={activeItem === 'skills'} onClick={this.handleItemClick} />
-          <Menu.Item name='education' active={activeItem === 'education'} onClick={this.handleItemClick} />
-          <Menu.Item name='experience' active={activeItem === 'experience'} onClick={this.handleItemClick} />
+          <Menu.Item as={Link} to='/skills' name='skills' active={activeItem === 'skills'} onClick={this.handleItemClick} />
+          <Menu.Item as={Link} to='/education' name='education' active={activeItem === 'education'} onClick={this.handleItemClick} />
+          <Menu.Item as={Link} to='/experience' name='experience' active={activeItem === 'experience'} onClick={this.handleItemClick} />
           <Menu.Menu position='right'>
-            <Menu.Item name='contact' active={activeItem === 'contact'} onClick={this.handleItemClick} />
+            <Menu.Item as={Link} to='/contact' name='contact' active={activeItem === 'contact'} onClick={this.handleItemClick} />
           </Menu.Menu>
         </Menu>
 
         <Segment>
-          <img src='/assets/images/wireframe/media-paragraph.png' />
+          <Routes />
         </Segment>
       </div>
     )
