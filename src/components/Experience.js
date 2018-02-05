1550b933ac8745494839b9b68538d14779b68b24
Jayson <jaysonmsaavedra@gmail.com>
2018-02-05 16:13:01 -0700

    Reverting it to not make any ajax requests because it can all be hosted in the web page

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,62 +3,6 @@ import { List, Header, Input }             from 'semantic-ui-react'
 import axios from 'axios'
 
 export default class Experience extends Component {
-  state = {
-    company1: '',
-    position1: '',
-    res1_1: '',
-    res1_2: '',
-    res1_3: '',
-    res1_4: '',
-    res1_5: '',
-    company2: '',
-    position2: '',
-    res2_1: '',
-    res2_2: '',
-    res2_3: '',
-    res2_4: '',
-    res2_5: '',
-    company3: '',
-    position3: '',
-    res3_1: '',
-    res3_2: '',
-    res3_3: '',
-    res3_4: '',
-    res3_5: ''
-  }
-
-  getJobInfo() {
-    axios.get('http://487057627162.dkr.ecr.us-west-2.amazonaws.com/rest_api:3006/api/experience')
-      .then(res => {
-        this.setState({
-          company1: res.data.data[0].company,
-          position1: res.data.data[0].position,
-          res1_1: res.data.data[0].res1,
-          res1_2: res.data.data[0].res2,
-          res1_3: res.data.data[0].res3,
-          res1_4: res.data.data[0].res4,
-          res1_5: res.data.data[0].res5,
-          company2: res.data.data[1].company,
-          position2: res.data.data[1].position,
-          res2_1: res.data.data[1].res1,
-          res2_2: res.data.data[1].res2,
-          res2_3: res.data.data[1].res3,
-          res2_4: res.data.data[2].res4,
-          res2_5: res.data.data[2].res5,
-          company3: res.data.data[2].company,
-          position3: res.data.data[2].position,
-          res3_1: res.data.data[2].res1,
-          res3_2: res.data.data[2].res2,
-          res3_3: res.data.data[2].res3,
-          res3_4: res.data.data[2].res4,
-          res3_5: res.data.data[2].res5,
-        })
-      })
-  }
-
-  componentDidMount() {
-    this.getJobInfo()
-  }
 
   render() {
     return(
@@ -66,33 +10,33 @@ export default class Experience extends Component {
         <Header as='h1'>Professional Experience</Header>
         <List>
           <List.Item>
-            <List.Header as='h3'>{this.state.company1}</List.Header>
-            <List.Description as='h4'>{this.state.position1} - August to Present</List.Description>
+            <List.Header as='h3'></List.Header>
+            <List.Description as='h4'> - August to Present</List.Description>
             <List.List>
-              <List.Content>● {this.state.res1_1}</List.Content>
-              <List.Content>● {this.state.res1_2}</List.Content>
-              <List.Content>● {this.state.res1_3}</List.Content>
+              <List.Content>● </List.Content>
+              <List.Content>● </List.Content>
+              <List.Content>● </List.Content>
             </List.List>
           </List.Item>
           <hr />
           <List.Item>
-            <List.Header as='h3'>{this.state.company2}</List.Header>
-            <List.Description as='h4'>{this.state.position2} - February to August</List.Description>
+            <List.Header as='h3'></List.Header>
+            <List.Description as='h4'> - February to August</List.Description>
               <List.List>
-                <List.Content>● {this.state.res2_1}</List.Content>
-                <List.Content>● {this.state.res2_2}</List.Content>
-                <List.Content>● {this.state.res2_3}</List.Content>
+                <List.Content>● </List.Content>
+                <List.Content>● </List.Content>
+                <List.Content>● </List.Content>
               </List.List>
           </List.Item>
           <hr />
           <List.Item>
-            <List.Header as='h3'>{this.state.company3}</List.Header>
-            <List.Description as='h4'>{this.state.position3} - June 2015</List.Description>
+            <List.Header as='h3'></List.Header>
+            <List.Description as='h4'> - June 2015</List.Description>
               <List.List>
-                <List.Content>● {this.state.res3_1}</List.Content>
-                <List.Content>● {this.state.res3_2}</List.Content>
-                <List.Content>● {this.state.res3_3}</List.Content>
-                <List.Content>● {this.state.res3_4}</List.Content>
+                <List.Content>● </List.Content>
+                <List.Content>● </List.Content>
+                <List.Content>● </List.Content>
+                <List.Content>● </List.Content>
               </List.List>
           </List.Item>
         </List>
--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -3,41 +3,15 @@ import { Image, Segment, Header, Responsive, List } from 'semantic-ui-react'
 import axios from 'axios'
 
 export default class Topbar extends Component {
-  state = {
-    first_name: '',
-    last_name: '',
-    email: '',
-    phone_number: ''
-  }
-
-  getUserInfo() {
-    axios.get('http://localhost:3006/api/personal')
-      .then(res =>
-        this.setState({
-          first_name: res.data.data[0].firstName,
-          last_name: res.data.data[0].lastName,
-          email: res.data.data[0].email,
-          phone_number: res.data.data[0].phoneNumber
-        }
-      )
-    )
-  }
-
-  componentDidMount() {
-    this.getUserInfo()
-  }
-
   handleOnUpdate = (e, { width }) => this.setState({ width })
 
   render() {
-    const { width } = this.state;
-    const textAlign = width >= Responsive.onlyComputer.minWidth ? 'right': 'center'
     return (
       <div className='Topbar'>
         <Header as='h2' className='Header' stackable>
-          <Image circular src='https://s3-us-west-2.amazonaws.com/my-resume-images/jaysonproshot.jpg' size='massive' />
+          <Image circular src='https://res.cloudinary.com/saavy1/image/upload/v1517409225/jaysonproshot_vcbkdd.jpg' size='massive' />
           <Header.Content>
-            {this.state.first_name} {this.state.last_name}
+            Jayson Saavedra
             <Header.Subheader>
               Junior Full Stack Developer
             </Header.Subheader>
@@ -48,12 +22,12 @@ export default class Topbar extends Component {
             <List>
               <List.Item>
                 <List.Icon name='phone' />
-                <List.Content><a href='tel:+1-760-213-5813'>{this.state.phone_number}</a></List.Content>
+                <List.Content><a href='tel:+1-760-213-5813'>(760) 213-5813</a></List.Content>
               </List.Item>
               <List.Item>
                 <List.Icon name='mail' />
                 <List.Content>
-                  <a href='mailto:jaysonmsaavedra@gmail.com'>{this.state.email}</a></List.Content>
+                  <a href='mailto:jaysonmsaavedra@gmail.com'>jaysonmsaavedra@gmail.com</a></List.Content>
               </List.Item>
             </List>
           </Header.Subheader>
